974b872469f4be8a73f53954222f09e5684af4a8
ElviraBudna <elvira.budna@gmail.com>
2023-04-05 21:18:22 +0300

    new HW

---
+++ src/components/FriendList.jsx
@@ -0,0 +1,20 @@
+import PropTypes from 'prop-types';
+
+export default function FriendList(friends) {
+  return (
+    <ul class="friend-list">
+      {/* {friends.map(friend => (
+        <li key={friend.id} class="item">
+          <span class="status">{friend.isOnline}</span>
+          <img
+            class="avatar"
+            src={friend.avatar}
+            alt="User avatar"
+            width="48"
+          />
+          <p class="name">{friend.name}</p>
+        </li>
+      ))} */}
+    </ul>
+  );
+}
---
+++ src/components/Statistics.jsx
@@ -0,0 +1,18 @@
+import PropTypes from 'prop-types';
+
+export default function Statistics({ title, data }) {
+  return (
+    <section class="statistics">
+      {title && <h2 class="title">{title}</h2>}
+
+      <ul class="stat-list">
+        {data.map(item => (
+          <li key={item.id} class="item">
+            <span class="label">{item.label}</span>
+            <span class="percentage">{item.percentage}%</span>
+          </li>
+        ))}
+      </ul>
+    </section>
+  );
+}
